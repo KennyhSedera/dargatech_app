3d9fb676b621f698bf1ffe9801a12289b5a79d33
kennyhsedera <kennyhsedera.com>
2025-02-24 13:49:45 +0300

    Port

--- resources/js/telegram.js
+++ resources/js/telegram.js
@@ -173,7 +173,7 @@ app.get("/", (req, res) => {
     res.send("Bot Telegram avec Express.js est en ligne !");
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORTBOT || 3001;
 app.listen(PORT, () => {
     console.log(`Serveur Express démarré sur http://localhost:${PORT}`);
 });
