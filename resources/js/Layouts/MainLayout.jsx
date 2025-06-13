c8807d4527810600a86d835aa446917a45c23223
sederakennyh <kennyhsedera@gmail.com>
2025-06-13 15:07:13 +0300

    rectification

--- resources/js/Layouts/MainLayout.jsx
+++ resources/js/Layouts/MainLayout.jsx
@@ -10,7 +10,6 @@ const MainLayout = ({ children }) => {
     return (
         <div className="relative h-screen w-full bg-gray-100 bg-[url('/images/bg.jpg')] bg-no-repeat bg-cover bg-center dark:bg-gray-900 dark:text-white flex items-center justify-center overflow-y-auto">
             <div className="absolute inset-0 bg-gradient-to-b from-black/60 to-black/80"></div>
-
             {children}
         </div>
 
